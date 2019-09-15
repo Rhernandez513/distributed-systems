3682aa4f62b3ca3b4758bb794b195d154eda948b
Robert David Hernandez <rherna57@mail.depaul.edu>
2019-09-15 14:05:54 -0500

    Stub out InetClients methods and dependent imports

--- src/main/java/web/InetClient.java
+++ src/main/java/web/InetClient.java
@@ -1,10 +1,16 @@
-// Based on InetClient, a client for InetServer. Elliot, after Huges, Shoffner, Winslow
+package web;// Based on web.InetClient, a client for web.InetServer. Elliot, after Huges, Shoffner, Winslow
 
 // DePaul CSC 435, Autumn 2019
 // Robert David Hernandez, rherna57@mail.depaul.edu
 
+import common.TextUtil;
+
 public class InetClient {
   public static void main(String[] args) {
+
+  }
+
+  static void getRemoteAddress(String name, String serverName) {
 
   }
 }

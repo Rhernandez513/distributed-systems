a6d5f2857e666079799aba64615c0930fa14fe57
Robert David Hernandez <rherna57@mail.depaul.edu>
2019-09-15 19:29:19 -0500

    Refactor fix Socket Close bug, squash TextUtil

--- src/main/java/web/InetClient.java
+++ src/main/java/web/InetClient.java
@@ -1,4 +1,4 @@
-package web;// Based on web.InetClient, a client for web.InetServer. Elliot, after Huges, Shoffner, Winslow
+package web;
 
 // DePaul CSC 435, Autumn 2019
 // Robert David Hernandez, rherna57@mail.depaul.edu
@@ -11,66 +11,68 @@ import java.net.Socket;
 
 public class InetClient {
 
-  private static final int PORT = 1565;
+  // We define a constant in the java style
+  private static final int PORT = 9001; // it's over 9000!
 
-  public static void main(String[] args) {
-    String serverName, name;
+  public static void main(String args[]) {
 
-    if (args.length < 1) { // always use brackets, always.  Not using brackets is a bug waiting to happen
-      serverName = "localhost";
-    } else {
-      serverName = args[0];
-    }
+    String machineName;
+
+    // These variables do not change over the  course of execution so they are assigned to final
+    // serverName was previously set as an if with no brackets, a bad idea in my eyes
+    final String serverName = (args.length < 1) ? "localhost" : args[0];
+    final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
     System.out.println("Robert David's Inet Client, 1.8.\n");
     System.out.println("Using server: " + serverName + ", Port: " + PORT);
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
     try {
       do {
         System.out.print("Enter a hostname or an IP address, (quit) to end: ");
         System.out.flush();
         // This will read user input from the keyboard
-        name = in.readLine();
-        if (name.indexOf("quit") < 0) {
-          getRemoteAddress(name, serverName);
+        // Attempting to read Remote Address of the machine name supplied
+        machineName = in.readLine();
+        if (machineName.indexOf("quit") < 0) {
+          getRemoteAddress(machineName, serverName);
         }
-      } while (name.indexOf("quit") < 0);
+      } while (machineName.indexOf("quit") < 0); // Loop until the user wants to exit
       System.out.println("Cancelled by user request.");
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
+  // this function will call out to the remote host "serverName" and ask that
+  // host to scan the local network for the provided "machineName"
+  // printing the result of the communication to the console
   static void getRemoteAddress(String name, String serverName) {
-    Socket socket;
-    BufferedReader fromServer;
-    PrintStream toServer;
     String textFromServer;
 
     try {
-      // Open connection to to host:PORT
-      socket = new Socket(serverName, PORT);
-      // Create filer IO streams for the socket
-      fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      toServer = new PrintStream(socket.getOutputStream());
-
+      // these vars can be declared and initialized in one line each
+      Socket sock = new Socket(serverName, PORT);
+      // Create IO streams for the socket
+      BufferedReader fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+      PrintStream toServer = new PrintStream(sock.getOutputStream());
       // Send hostname or IP to server
       toServer.println(name);
-      toServer.flush(); // I would never advise putting two statements on one line
-
-      // Read two or three lines of response from the server,
-      // and block while synchronously waiting:
-      for(int i = 0; i <= 3; ++i) {
-        // This will
+      toServer.flush(); // don't put two statements on one line
+      // Read three lines of response from the server, and block while synchronously waiting:
+      for (int i = 1; i <= 3; i++) {
+        // Don't assign vars inside loops if we can help it
         textFromServer = fromServer.readLine();
-        if (textFromServer != null) { // always use brackets, always
+        // ALWAYS use brackets
+        if (textFromServer != null) {
           System.out.println(textFromServer);
         }
-        socket.close();
       }
+      // Here we close the external resource we acquired
+      sock.close();
     } catch (IOException e) {
       System.out.println("Socket error.");
       e.printStackTrace();
     }
   }
 }
+
--- src/main/java/web/InetServer.java
+++ src/main/java/web/InetServer.java
@@ -1,11 +1,10 @@
-package web;// Based on web.InetServer. Elliot, after Huges, Shoffner, Winslow
+package web; // Based on InetServer Elliot, after Huges, Shoffner, Winslow
+
 // DePaul CSC 435, Autumn 2019
 // Robert David Hernandez, rherna57@mail.depaul.edu
 
 // I prefer to be explicit rather than implicit, but I can see value in wildcard imports
 // If the list is large enough to make it a distraction
-
-import common.TextUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,33 +15,34 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-class Worker extends Thread {
 
-  private Socket socket;
-  // Moved variables used in multiple methods to class scope in the object-oriented style
-  private PrintStream outStream;
+class Worker extends Thread {
+  private Socket sock;
 
   Worker(Socket s) {
-    this.socket = s;
+    sock = s;
   }
 
   public void run() {
+    // tried moving this to class level to avoid passing it around but it didn't work out, undoing
+    PrintStream outStream = null;
     // Assignment to null is unnecessary in this instance
     // we could even move this var to class level for consistency, but it isn't used outside this method
-    BufferedReader inReader;
-
+    BufferedReader in;
     try {
-      inReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      outStream = new PrintStream(socket.getOutputStream());
+      in = new BufferedReader
+              (new InputStreamReader(sock.getInputStream()));
+      outStream = new PrintStream(sock.getOutputStream());
       try {
-        final String name = inReader.readLine();
+        String name;
+        name = in.readLine();
         System.out.println("Looking up " + name);
-        printRemoteAddress(name);
+        printRemoteAddress(name, outStream);
       } catch (IOException e) {
         System.out.println("Server read error");
         e.printStackTrace();
       }
-      socket.close();
+      sock.close();
     } catch (IOException e) {
       System.out.println(e);
     }
@@ -50,38 +50,44 @@ class Worker extends Thread {
 
   // We aren't using another module inside the same package, so static use of these
   // two methods with default visibility doesn't add value
-  private void printRemoteAddress(String name) {
+  private void printRemoteAddress(String name, PrintStream out) {
     try {
-      outStream.println("Looking up " + name + "...");
+      out.println("Looking up " + name + "...");
       InetAddress machine = InetAddress.getByName(name);
-      outStream.println("Host name : " + machine.getHostName());
-      outStream.println("Host IP : " + TextUtil.toText(machine.getAddress()));
-    } catch (UnknownHostException e) {
-      outStream.println("Failed to attempt to look up " + name);
+      out.println("Host name : " + machine.getHostName());
+      out.println("Host IP : " + toText(machine.getAddress()));
+    } catch (UnknownHostException ex) {
+      out.println("Failed in atempt to look up " + name);
     }
+  }
+
+  // Makes portable for 128 bit format
+  static String toText(byte ip[]) {
+    final StringBuffer result = new StringBuffer();
+    for (int i = 0; i < ip.length; ++i) {
+      if (i > 0) {
+        result.append(".");
+      }
+      result.append(0xff & ip[i]);
+    }
+    return result.toString();
   }
 }
 
 public class InetServer {
-  // Made these hard-coded values in the style of constants in Java to show intent and provide for
-  // readability
+  // Made these hard-coded values in the style of constants in Java to show intent and provide for readability
   private static final int Q_LENGTH = 6;
-  private static final int PORT = 1565;
+  private static final int PORT = 9001; // it's over 9000!
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String a[]) throws IOException {
     Socket socket;
-
-    final ServerSocket serversocket = new ServerSocket(PORT, Q_LENGTH);
-
+    ServerSocket servsock = new ServerSocket(PORT, Q_LENGTH);
     System.out.println("Robert David's Inet server 1.8 starting up, listening on port " + PORT + "\n");
     while (true) {
-      socket = serversocket.accept();
-      // I did an internet search and found that .start() will spawn a new thread
-      // and then call .run() where a direct call to .run() will not spawn a thread
+      socket = servsock.accept();
+      // .start() will spawn a new thread and then call .run()
       new Worker(socket).start();
     }
   }
 }
-
-// EOF
 

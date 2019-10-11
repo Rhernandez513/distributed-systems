a248847edab9ee46841fab8cd1fb33f9ccc9c900
Robert David Hernandez <rherna57@mail.depaul.edu>
2019-10-10 20:58:15 -0500

    Find directory of running JAR

--- webserver/src/main/java/Solution.java
+++ webserver/src/main/java/Solution.java
@@ -1,5 +1,17 @@
+import java.io.File;
+import java.net.URISyntaxException;
+
 public class Solution {
   public static void main(String[] args) {
-    System.out.println("hello world!");
+    try {
+      System.out.println(getDirectoryOfJAR());
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+  }
+
+  // https://stackoverflow.com/a/320595
+  private static String getDirectoryOfJAR() throws URISyntaxException {
+    return new File(Solution.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
   }
 }

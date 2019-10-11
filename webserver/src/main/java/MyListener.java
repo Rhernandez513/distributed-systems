d76bf6d00b46652a0e758199ac4a80d53fa3f36c
Robert David Hernandez <rherna57@mail.depaul.edu>
2019-10-10 21:47:21 -0500

    basic web echo server

---
+++ webserver/src/main/java/MyListener.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyListener {
+
+  private static final int Q_LENGTH = 6; /* Number of requests for OpSys to queue */
+  private static final int PORT = 2540;
+
+  public static void main(String[] args) {
+    try {
+
+      ServerSocket servsock = new ServerSocket(PORT, Q_LENGTH);
+      while (true) {
+        // Will block on .accept()
+        new Thread(new ListenerWorker(servsock.accept())).start();
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
+class ListenerWorker implements Runnable {
+  private Socket socket;
+  ListenerWorker (Socket socket) {
+    this.socket = socket;
+  }
+
+  public void run() {
+    try {
+      BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      String line = input.readLine().trim();
+
+      System.out.println(line);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+

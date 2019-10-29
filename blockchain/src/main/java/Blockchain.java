ff44ecb133073fa0c2b1f2f286889e94919fcad1
Robert David Hernandez <rherna57@mail.depaul.edu>
2019-10-28 21:42:55 -0500

    Basic hello world and hello test implemented

---
+++ blockchain/src/main/java/Blockchain.java
@@ -0,0 +1,5 @@
+public class Blockchain {
+  public static void main(String [] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ blockchain/src/test/java/BlockchainTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class BlockchainTest {
+
+  @BeforeAll
+  public static void setUp() { }
+
+  @Test
+  public void helloTest() {
+    System.out.println("Hello Test!");
+  }
+}
